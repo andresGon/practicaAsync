bc0156a1cf60de2c28f80e5da8ffb02cc30bb2c8
Andres Gonzalez <andres.gonzalez@dasigno.com>
2020-12-17 09:55:08 -0500

    input haciendo llamado

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-const API = 'https://rickandmortyapi.com/api/character';
+const API = 'https://rickandmortyapi.com/api/character/';
 var parrafo = document.getElementById("texto");
 var imagen = document.getElementById("imagen");
+var especie = document.getElementById("especie");
+var select = document.getElementById("selector");
+var input = document.getElementById("inputTxt");
+var numPerosnaje = 0;
 
-const llamado = async (API) =>{
+
+
+const llamado = async (API, numPer) =>{
     try{
         const respuesta = await fetch(API);
         const texto = await respuesta.json();
-        let consolaTexto = texto.results[0].name;
-        let origenImagen = texto.results[0].image;
+        let consolaTexto = texto.results[numPer].name;
+        let origenImagen = texto.results[numPer].image;
+        let especieTexto = texto.results[numPer].species;
         parrafo.textContent = consolaTexto;
         imagen.src = origenImagen;
+        especie.textContent = especieTexto;
+        console.log(texto.info.count);
     } catch(error){
         console.log(error);
     }
 }
 
-llamado(API);
+
+// select.addEventListener('change', (event) => {
+//     llamado(API, event.target.value);
+// });
+
+input.addEventListener('change', (event)=>{
+    llamado(API, event.target.value);
+
+});
+
+
+
 
 
 
